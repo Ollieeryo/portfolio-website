402ff0d2a67efc9430f9c1eb7f61434d353c5028
Ollieeryo <ollieeryo@gmail.com>
2023-04-19 21:49:40 +0800

    feat: edit resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -118,7 +118,7 @@ export const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://docs.google.com/document/d/1vPW713U6F-t7zBtjkZKcKsrF9os9yDIWR5BHzrgsRIQ/edit?usp=sharing"
+              href="https://drive.google.com/file/d/1zihV-JefzZphZxg4m5Ty6oYnrwF61Pfr/view?usp=sharing"
               target="_blank"
               rel="noreferrer noopener"
             >
